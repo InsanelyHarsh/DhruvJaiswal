761f49c2580a5c498dbcb4b5d6db10c9b7d8be60
Harsh M. Yadav <98019495+InsanelyHarsh@users.noreply.github.com>
2023-03-03 13:42:51 +0530

    Initial Commit

---
+++ Dhruv Jaiswal/Dhruv_JaiswalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dhruv_JaiswalApp.swift
+//  Dhruv Jaiswal
+//
+//  Created by Harsh Yadav on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Dhruv_JaiswalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
